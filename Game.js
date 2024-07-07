ae4771075f7e151dc01a9bf75323057a1b3aa823
Vicente Aranda <vicente.aranda@protonmail.com>
2024-07-07 18:20:17 -0400

    📦 feat: Added balls in positions.

--- Game.js
+++ Game.js
@@ -1,10 +1,12 @@
 class Game {
+	topLimit = height / 2 - TABLE_WIDTH / 2 + CUSHION_WIDTH;
+	bottomLimit = height / 2 + TABLE_WIDTH / 2 - CUSHION_WIDTH;
+	leftLimit = width / 2 - TABLE_LENGTH / 2 + CUSHION_WIDTH;
+	rightLimit = width / 2 + TABLE_LENGTH / 2 - CUSHION_WIDTH;
+
 	constructor() {
-		this.redBalls = [];
-		for (let i = 0; i < 15; i++) {
-			this.redBalls.push(new Ball(width / 2, height / 2, "red"));
-		}
 		this.placeColorBalls();
+		this.placeRedBalls();
 
 		this.table = new Table();
 		cueBall = new Ball(width / 2, 200, "white");
@@ -21,20 +23,87 @@ class Game {
 		this.draw();
 	}
 
-	placeRedBalls() {}
+	placeRedBalls(mode) {
+		this.redBalls = [];
+		if (mode === "random") {
+			for (let i = 0; i < 15; i++) {
+				this.redBalls.push(
+					new Ball(
+						random(this.leftLimit, this.rightLimit),
+						random(this.bottomLimit, this.topLimit),
+						"red"
+					)
+				);
+			}
+		} else {
+			let startHeight = 0;
+			let gap = 2;
+			let baseX = width / 2 + TABLE_LENGTH / 4 + gap,
+				baseY = height / 2;
+			let x, y;
+			for (let i = 1; i <= 5; i++) {
+				// Columns
+				baseX += gap;
+				baseY = height / 2;
+				for (let j = 1; j <= i; j++) {
+					// Rows
+					baseY += gap;
+					x = baseX + BALL_DIAMETER * i;
+					y = baseY + startHeight + BALL_DIAMETER * (j - 1);
+					this.redBalls.push(new Ball(x, y, "red"));
+				}
+				startHeight -= BALL_DIAMETER / 2 + gap;
+			}
+		}
+	}
 
-	placeColorBalls() {
-		this.colorBalls = [
-			new Ball(ZONE_D_LINE, height / 2, "orange"),
-			new Ball(ZONE_D_LINE, height / 2 - TABLE_WIDTH / 6, "green"),
-			new Ball(ZONE_D_LINE, height / 2 + TABLE_WIDTH / 6, "yellow"),
-			new Ball(width / 2, height / 2, "blue"),
-			new Ball(width / 2 + TABLE_LENGTH / 4, height / 2, "purple"),
-			new Ball(
-				width / 2 + TABLE_LENGTH / 2 - TABLE_LENGTH / 8,
-				height / 2,
-				"black"
-			),
-		];
+	placeColorBalls(mode) {
+		if (mode === "random") {
+			this.colorBalls = [
+				new Ball(
+					random(this.leftLimit, this.rightLimit),
+					random(this.bottomLimit, this.topLimit),
+					"orange"
+				),
+				new Ball(
+					random(this.leftLimit, this.rightLimit),
+					random(this.bottomLimit, this.topLimit),
+					"green"
+				),
+				new Ball(
+					random(this.leftLimit, this.rightLimit),
+					random(this.bottomLimit, this.topLimit),
+					"yellow"
+				),
+				new Ball(
+					random(this.leftLimit, this.rightLimit),
+					random(this.bottomLimit, this.topLimit),
+					"blue"
+				),
+				new Ball(
+					random(this.leftLimit, this.rightLimit),
+					random(this.bottomLimit, this.topLimit),
+					"purple"
+				),
+				new Ball(
+					random(this.leftLimit, this.rightLimit),
+					random(this.bottomLimit, this.topLimit),
+					"black"
+				),
+			];
+		} else {
+			this.colorBalls = [
+				new Ball(ZONE_D_LINE, height / 2, "orange"),
+				new Ball(ZONE_D_LINE, height / 2 - TABLE_WIDTH / 6, "green"),
+				new Ball(ZONE_D_LINE, height / 2 + TABLE_WIDTH / 6, "yellow"),
+				new Ball(width / 2, height / 2, "blue"),
+				new Ball(width / 2 + TABLE_LENGTH / 4, height / 2, "purple"),
+				new Ball(
+					width / 2 + TABLE_LENGTH / 2 - TABLE_LENGTH / 8,
+					height / 2,
+					"black"
+				),
+			];
+		}
 	}
 }
